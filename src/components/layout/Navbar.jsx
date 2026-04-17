8aacd0ba3c28ad10870e18e97b17435bc1b4aeac
El Amraoui Mounir <mounir.el.amraoui186@gmail.com>
2026-04-16 12:23:52 -1200

    m

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Navbar.css';
+import { Home, Users, Calendar, LayoutDashboard, Compass } from 'lucide-react';
 
 const Navbar = ({ onMenuToggle, isSidebarOpen }) => {
   const activeClass = ({ isActive }) => (isActive ? 'nav-link active' : 'nav-link');
@@ -18,17 +19,25 @@ const Navbar = ({ onMenuToggle, isSidebarOpen }) => {
         </NavLink>
 
         <nav className="navbar-nav">
-          <NavLink to="/" className={activeClass} end>
-            Home
+          <NavLink to="/" className={activeClass} end title="Home">
+            <Home size={20} />
+            <span className="nav-label">Home</span>
           </NavLink>
-          <NavLink to="/vendors" className={activeClass}>
-            Vendors
+          <NavLink to="/explore" className={activeClass} title="Explore">
+            <Compass size={20} />
+            <span className="nav-label">Explore</span>
           </NavLink>
-          <NavLink to="/experiences" className={activeClass}>
-            Experiences
+          <NavLink to="/vendors" className={activeClass} title="Vendors">
+            <Users size={20} />
+            <span className="nav-label">Vendors</span>
           </NavLink>
-          <NavLink to="/dashboard" className={activeClass}>
-            Dashboard
+          <NavLink to="/experiences" className={activeClass} title="Experiences">
+            <Calendar size={20} />
+            <span className="nav-label">Experiences</span>
+          </NavLink>
+          <NavLink to="/dashboard" className={activeClass} title="Dashboard">
+            <LayoutDashboard size={20} />
+            <span className="nav-label">Dashboard</span>
           </NavLink>
         </nav>
 
